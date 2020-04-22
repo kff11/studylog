70d10cbd6c871110a19fde395962929bbdefb802
kff11 <55500009+kff11@users.noreply.github.com>
2020-04-22 23:31:43 +0900

    `RDS Connect`

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const http = require('http');
+const db = require('./config/db');
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('소문난 김가네팀')
-})
+});
+
+app.get('/api/db', (req, res) => {
+    db.query("select * from RDStest", (err, data) => {
+        if (!err) {
+            res.send(data);
+
+        } else {
+            console.log(err);
+            res.send(err);
+        }
+    })
+});
 
 http.createServer(app).listen(process.env.PORT || 4000);
