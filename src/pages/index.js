cec1a9772d50233d9d7d20692bca8ee7b9aa37d0
kimjihyunKOR <12jihyun28@gmail.com>
2020-06-08 01:46:13 +0900

    I cleaned up components of 'Board'. I should edit drawer, and should add comment rank list! we have to add props and list system!

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export {default as Login} from './login';
+// export {default as Diary} from './diary';
+export {default as Write} from './write';
+export {default as About} from './about';
+export {default as Main} from './main';
+export {default as Mentoring} from './mentoring';
+export {default as Profile} from './profile';
